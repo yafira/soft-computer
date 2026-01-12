01f218de079aea4533c264bcd45eb53be5ae59f6
Yafira <ym3317@nyu.edu>
2026-01-11 21:47:56 -0500

    feat: render timeline from punched logs

---
+++ src/app/timeline/page.js
@@ -0,0 +1,9 @@
+import TimelineList from "../../components/TimelineList";
+
+export default function TimelinePage() {
+  return (
+    <main className="wrap">
+      <TimelineList />
+    </main>
+  );
+}
